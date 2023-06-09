3976d6c953035f56425e6826638646fe2b8364cd
@guustavocl <gustavo@gus.sh>
2023-06-09 14:55:02 -0300

    fix env variable

--- src/services/CustomAxios.ts
+++ src/services/CustomAxios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
-
 export const CustomAxios = axios.create({
-  baseURL: process.env.EXPRESS_API_URL || "http://127.0.0.1:3100",
+  baseURL: process.env.NEXT_PUBLIC_EXPRESS_API_URL || "http://127.0.0.1:3100",
   headers: {
     "Content-Type": "application/json",
   },
